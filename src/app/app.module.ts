44dbc90f50073a9ed8ee650a8fb803ccfde24d36
axelfoxII <asfoxii@gmail.com>
2022-04-26 19:33:04 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SearchComponent } from './pages/search/search.component';
+import { NavbarComponent } from './pages/shared/navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ArtistaComponent } from './pages/artista/artista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    NavbarComponent,
+    ArtistaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
